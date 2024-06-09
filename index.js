ac349b6ce580d33de84c665eab5e2babc3e8f77c
Muhammad Ahmed Mustafa <muhammadahmedmughal24@gmail.com>
2024-06-09 13:23:32 +0500

    project completed

--- index.js
+++ index.js
@@ -221,39 +221,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-// menue
-// function toggleMenu() {
-//     var menuIcon = document.querySelector('.fa-bars');
-//     var menuLinks = document.querySelector('.menu-links9');
-//     if (window.innerWidth <= 550) {
-//       menuIcon.style.display = 'block';
-//       menuLinks.style.display = 'none';
-//     } else {
-//       menuIcon.style.display = 'none';
-//       menuLinks.style.display = 'none';
+// link scroll
+// document.addEventListener('DOMContentLoaded', function() {
+//     var pcLink = document.querySelector('a[href="#PC"]');
+//     if (pcLink) {
+//         pcLink.addEventListener('click', function(event) {
+//             event.preventDefault(); // Prevent default anchor click behavior
+//             var middleOfPage = document.documentElement.scrollHeight / 2;
+//             window.scrollTo({
+//                 top: middleOfPage,
+//                 behavior: 'smooth'
+//             });
+//         });
 //     }
-//   }
-  
-//   // Call toggleMenu function initially to set the initial state
-//   toggleMenu();
-  
-//   // Add event listener for window resize
-//   window.addEventListener('resize', toggleMenu);
-  
-//   // Add click event listener for menu icon
-//   document.querySelector('.fa-bars').addEventListener('click', function() {
-//     var menuLinks = document.querySelector('.menu-links9');
-//     if (menuLinks.style.display === 'block') {
-//       menuLinks.style.display = 'none';
-//     } else {
-//       menuLinks.style.display = 'block';
-//     }
-//   });
-  
-  
-
-
-
-
-
+// });
